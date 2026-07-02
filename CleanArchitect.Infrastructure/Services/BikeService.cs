d339e13a5e921f5d8ebeec387b6a3aa11a5cf1a7
HunnyMun <minhanharmy2130@gmail.com>
2026-07-02 12:41:26 +0700

    Add files via upload

---
+++ CleanArchitect.Infrastructure/Services/BikeService.cs
@@ -0,0 +1,122 @@
+﻿using CleanArchitect.Application.DTOs.BikeShop;
+using CleanArchitect.Application.Interfaces;
+using CleanArchitect.Common.Configs;
+using CleanArchitect.Domain.Entities.BikeShop;
+using CleanArchitect.Infrastructure.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitect.Infrastructure.Services
+{
+    public class BikeService : IBikeService
+    {
+        private readonly EBikeShopDbContext _context;
+        public BikeService(EBikeShopDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Bike> Create(BikeDTO bikeDTO)
+        {
+            var countBikes = await _context.Bikes.CountAsync();
+            var bike = new Bike
+            {
+                Name = bikeDTO.Name.Trim(),
+                BrandName = bikeDTO.BrandName.Trim(),
+                CategoryId = bikeDTO.CategoryId,
+                //Category = bikeDTO.Category.Trim(),
+                Description = bikeDTO.Description?.Trim(),
+                ImageName = bikeDTO.ImagePath,
+                Year = bikeDTO.Year,
+                Position = ++countBikes
+            };
+            _context.Bikes.Add(bike);
+            return bike;
+
+        }
+
+        public async Task<bool> Delete(Guid idBike)
+        {
+            var isOK = false;
+            try
+            {
+                var bike = await _context.Bikes.FindAsync(idBike);
+                if (bike != null)
+                {
+                    _context.Bikes.Remove(bike);
+                    await _context.SaveChangesAsync();
+                    isOK = true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return isOK;
+        }
+
+        public async Task<BikeDTO[]> GetAll()
+        {
+            var bikes = new BikeDTO[] { };
+            try
+            {
+                bikes = await _context.Bikes
+                    .Select(b => new BikeDTO(b))
+                    .ToArrayAsync();
+            }
+            catch
+            {
+
+            }
+            return bikes;
+        }
+
+        public async Task<BikeDTO> GetById(Guid idBike)
+        {
+            var dtoBike = new BikeDTO();
+            try
+            {
+                dtoBike = await _context.Bikes
+                    .Where(b => b.Id == idBike)
+                    .Select(b => new BikeDTO(b))
+                    .SingleOrDefaultAsync();
+            }
+            catch
+            {
+
+            }
+            return dtoBike;
+        }
+
+        public List<Bike> GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Update(BikeDTO bikeDTO)
+        {
+            var isOK = false;
+            try
+            {
+                var bike = await _context.Bikes.FindAsync(bikeDTO.Id);
+                if (bike != null)
+                {
+                    bike.Name = bikeDTO.Name.Trim();
+                    bike.BrandName = bikeDTO.BrandName.Trim();
+                    bike.Year = bikeDTO.Year;
+                    bike.CategoryId = bikeDTO.CategoryId;
+                    //bike.Category = bikeDTO.Category.Trim();
+                    bike.Description = bikeDTO?.Description?.Trim();
+                    bike.ImageName = bikeDTO?.ImagePath;
+
+                    await _context.SaveChangesAsync();
+                    isOK = true;
+                }
+            }
+            catch
+            {
+
+            }
+            return isOK;
+        }
+    }
+}
---
+++ CleanArchitect.Infrastructure/Services/CategoryService.cs
@@ -0,0 +1,131 @@
+﻿using CleanArchitect.Application.DTOs.BikeShop;
+using CleanArchitect.Application.Interfaces;
+using CleanArchitect.Domain.Entities.BikeShop;
+using CleanArchitect.Infrastructure.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitect.Infrastructure.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly EBikeShopDbContext _context;
+        public CategoryService(EBikeShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category?> Create(CategoryDTO categoryDTO)
+        {
+            try
+            {
+                var countCategory = await _context.Categories.CountAsync();
+                //category.Position = ++countCategory;
+                //_context.Add(category);
+                var newCategory = new Category
+                {
+                    //Id = Guid.NewGuid(),
+                    Name = categoryDTO.Name.Trim(),
+                    Description = categoryDTO.Description?.Trim(),
+                    Position = ++countCategory
+                };
+                _context.Categories.Add(newCategory);
+                await _context.SaveChangesAsync();
+                return newCategory;
+            }
+            catch (Exception)
+            {
+            }
+            return null;
+        }
+
+        public async Task<bool> Delete(Guid idCategory)
+        {
+            var isOK = false;
+            try
+            {
+                var category = await _context.Categories.FindAsync(idCategory);
+                if (category != null)
+                {
+                    _context.Categories.Remove(category);
+                }
+
+                await _context.SaveChangesAsync();
+                isOK = true;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return isOK;
+        }
+
+        public async Task<CategoryDTO[]> GetAll()
+        {
+            var cateDTOs = new CategoryDTO[]{ };
+            try
+            {
+                cateDTOs = await _context.Categories
+                    .Select(c => new CategoryDTO
+                    {
+                        Id = c.Id,
+                        Name = c.Name,
+                        Description = c.Description,
+                        Position = c.Position
+                    })
+                    .ToArrayAsync();
+            }
+            catch (Exception ex)
+            { }
+            return cateDTOs;
+        }
+
+        public async Task<CategoryDTO?> GetById(Guid idCategory)
+        {
+            var dtoCategory = new CategoryDTO();
+            try
+            {
+                dtoCategory = await _context.Categories
+                    .Where(c => c.Id == idCategory)
+                    .Select(c => new CategoryDTO
+                    {
+                        Id = c.Id,
+                        Name = c.Name,
+                        Description = c.Description,
+                        Position = c.Position
+                    })
+                    .SingleOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return dtoCategory;
+        }
+
+        public List<Category> GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Update(CategoryDTO categoryDTO)
+        {
+            bool isOK = false;
+            try
+            {
+                var oldCate = await _context.Categories.FindAsync(categoryDTO.Id);
+                if (oldCate != null)
+                {
+                    oldCate.Name = categoryDTO.Name.Trim();
+                    oldCate.Description = categoryDTO.Description?.Trim();
+                    await _context.SaveChangesAsync();
+                    isOK = true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return isOK;
+        }
+    }
+}
